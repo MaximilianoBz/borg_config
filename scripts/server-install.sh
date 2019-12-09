6f8c85926b6edc64f8d1a2c10367331d82e8deec
Pablo Ramos <pramos@lunix.com.ar>
2019-12-09 12:06:25 -0300

    Script de instalacion

--- scripts/server-install.sh
+++ scripts/server-install.sh
@@ -13,17 +13,17 @@
 #Es necesario al menos la version 1.1 (aqui instalamos descargando el binario)
 #En buster se puede instalar por apt, en stretch activando stretch-backports
 echo "Instalando borgbackup"
-curl https://github.com/borgbackup/borg/releases/download/1.1.10/borg-linux64 -o /usr/local/bin/borg
+curl -sL https://github.com/borgbackup/borg/releases/download/1.1.10/borg-linux64 -o /usr/local/bin/borg
 chown root:root /usr/local/bin/borg
 chmod 755 /usr/local/bin/borg
 
 #Descargar borg_config
 echo "Instalando script de Lunix: borg_config"
-curl https://gitlab.lunix.com.ar/pramos/borg_config/raw/master/borg_config?inline=false -o /usr/local/sbin/borg_config
+curl -sL https://gitlab.lunix.com.ar/pramos/borg_config/raw/master/borg_config?inline=false -o /usr/local/sbin/borg_config
 chmod +x /usr/local/sbin/borg_config
 mkdir -p /etc/lunix/borg/.ssh
 chmod 600 -R /etc/lunix/borg
-curl https://gitlab.lunix.com.ar/pramos/borg_config/raw/master/borgcron.conf?inline=false -o /etc/lunix/borg/borgcron.conf
+curl -sL https://gitlab.lunix.com.ar/pramos/borg_config/raw/master/borgcron.conf?inline=false -o /etc/lunix/borg/borgcron.conf
 
 #Agregar usuario y home de borg
 echo "Generando usuario y home para borg"
