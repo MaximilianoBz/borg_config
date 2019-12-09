04ed6fea155dff5bc6726873eb1bf098516c6db1
Pablo Ramos <pramos@lunix.com.ar>
2019-12-09 12:40:03 -0300

    Script de instalacion

--- scripts/server-install.sh
+++ scripts/server-install.sh
@@ -21,7 +21,7 @@ fi
 #Es necesario al menos la version 1.1 (aqui instalamos descargando el binario)
 #En buster se puede instalar por apt, en stretch activando stretch-backports
 echo "Instalando borgbackup"
-if  [ ${#CURL} =~ 0 ]; then
+if  [ ! ${#CURL} -eq 0 ]; then
     curl -sL https://github.com/borgbackup/borg/releases/download/1.1.10/borg-linux64 -o /usr/local/bin/borg
 else
     wget -q https://github.com/borgbackup/borg/releases/download/1.1.10/borg-linux64 -O /usr/local/bin/borg
@@ -31,7 +31,7 @@ chmod 755 /usr/local/bin/borg
 
 #Descargar borg_config
 echo "Instalando script de Lunix: borg_config"
-if  [ ${#CURL} =~ 0 ]; then
+if  [ ! ${#CURL} -eq 0 ]; then
     curl -sL https://gitlab.lunix.com.ar/pramos/borg_config/raw/master/borg_config?inline=false -o /usr/local/sbin/borg_config
     curl -sL https://gitlab.lunix.com.ar/pramos/borg_config/raw/master/borgcron.conf?inline=false -o /etc/lunix/borg/borgcron.conf
 else
