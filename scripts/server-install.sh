6214ccca02be2a8f0532ed8a4096aa21333f0480
Pablo Ramos <pramos@lunix.com.ar>
2019-12-09 12:56:06 -0300

    Script de instalacion

--- scripts/server-install.sh
+++ scripts/server-install.sh
@@ -31,6 +31,7 @@ chmod 755 /usr/local/bin/borg
 
 #Descargar borg_config
 echo "Instalando script de Lunix: borg_config"
+mkdir -p /etc/lunix/borg/.ssh
 if  [ ! ${#CURL} -eq 0 ]; then
     curl -sL https://gitlab.lunix.com.ar/pramos/borg_config/raw/master/borg_config?inline=false -o /usr/local/sbin/borg_config
     curl -sL https://gitlab.lunix.com.ar/pramos/borg_config/raw/master/borgcron.conf?inline=false -o /etc/lunix/borg/borgcron.conf
@@ -39,7 +40,6 @@ else
     wget -q https://gitlab.lunix.com.ar/pramos/borg_config/raw/master/borgcron.conf?inline=false -O /etc/lunix/borg/borgcron.conf
 fi
 chmod +x /usr/local/sbin/borg_config
-mkdir -p /etc/lunix/borg/.ssh
 chmod 600 -R /etc/lunix/borg
 
 #Agregar usuario y home de borg
